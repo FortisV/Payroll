7a208ad9bafb98e9fdc1f5e4c3d74def01056c20
fortisvariano <vf41922@mail.harpercollege.edu>
2025-02-04 22:20:40 -0600

    Added tests for AppTest.java

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,11 +4,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  // You can put your JUnit tests here
-  // Feel free to create other files, as needed, to organize your tests
+  @Test
+  void grossPayTest() {
+    assertEquals(192, App.getGrossPay(12, 16));
+  }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void grossPayOTTest() {
+    assertEquals(880, App.getGrossPay(50, 16));
+  }
+
+  @Test
+  void healthInsuranceTestNoDependents() {
+    assertEquals(15, App.getHealthInsuranceDeduction(0));
+  }
+
+  @Test
+  void healthInsuranceTest4Dependents() {
+    assertEquals(35, App.getHealthInsuranceDeduction(4));
+  }
+
+  @Test
+  void lifeInsuranceTest1() {
+    assertEquals(0, App.getLifeInsurancePlanDeduction(1));
+  }
+
+  @Test
+  void lifeInsuranceTest2() {
+    assertEquals(5, App.getLifeInsurancePlanDeduction(2));
+  }
+
+  @Test
+  void lifeInsuranceTest3() {
+    assertEquals(10, App.getLifeInsurancePlanDeduction(3));
+  }
+
+  @Test
+  void lifeInsuranceTest4() {
+    assertEquals(15, App.getLifeInsurancePlanDeduction(4));
+  }
+
+  @Test
+  void netTaxTest() {
+    assertEquals(15, App.getNetTax(5, 5, 5));
+  }
+
+  @Test
+  void netFeeTest() {
+    assertEquals(15, App.getNetFee(5, 5, 5));
   }
 }
